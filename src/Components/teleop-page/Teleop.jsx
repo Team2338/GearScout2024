b5cb9cf99418901980103cdaf8f9dc5d40859e45
parallax9999 <geetygam@proton.me>
2024-01-17 19:15:44 -0600

    Preliminary trap added

--- src/Components/teleop-page/Teleop.jsx
+++ src/Components/teleop-page/Teleop.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './Teleop.scss'
+import Trap from "./Trap";
 
 
 export default function Teleop() {
     return (
         <>
         <h1>placeholder</h1>
+        <Trap />
         </>
     )
 }
