72e24181fe3e96239e9f23a1c679c113c0a63769
parallax9999 <geetygam@proton.me>
2024-01-18 19:16:09 -0600

    Made Amp code

--- src/Components/auto-page/Auto.jsx
+++ src/Components/auto-page/Auto.jsx
@@ -2,11 +2,14 @@
 import { Button } from '@mui/material';
 import React from 'react';
 import Leave from './leave';
+import AutoSpeaker from './AutoSpeaker';
 import './Auto.scss'
 
 export default function Auto() {
     return (
         <>
+        <Leave />
+        <AutoSpeaker />
         </>
     )
 }
