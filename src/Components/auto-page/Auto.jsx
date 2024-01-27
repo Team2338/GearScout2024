e311f43b5c14919bf0160f18629cdfae3f35e83d
parallax9999 <geetygam@proton.me>
2024-01-27 15:33:30 -0600

    Removed info buttons and optimized padding

--- src/Components/auto-page/Auto.jsx
+++ src/Components/auto-page/Auto.jsx
@@ -3,17 +3,12 @@ import React from 'react';
 import Leave from './leave';
 import Counter from '../shared/counter/Counter';
 import './Auto.scss'
-import InfoButton from '../shared/info-buttons/InfoButtons';
 import { useDispatch } from 'react-redux';
 import { setLowGoal, setHighGoal } from '../../app/Actions';
 
 export default function Auto() {
     const AutoSpeaker = "AutoSpeaker"
     const AutoAmp = "AutoAmp"
-    const LeaveInfoText = 'Click yes if the robot left their starting zone during Autonomous.'
-    const AutoSpeakerInfoText = 'Click the + and - buttons to note how many Notes the robot scored in the SPEAKER during Auto.'
-    const AutoAmpInfoText = 'Click the + and - buttons to note how many Notes the robot scored in the AMP during Auto.'
-
     const dispatch = useDispatch()
 
     const lowGoal = (counter) => dispatch(setLowGoal('auto', counter))
@@ -23,14 +18,11 @@ export default function Auto() {
     return (
         <>
         <h1>Auto</h1>
-        <h3>Leave</h3>
-        <InfoButton text={LeaveInfoText} />
+        <h3 className='padding'>Leave</h3>
         <Leave />
-        <h3>Speaker</h3>
-        <InfoButton text={AutoSpeakerInfoText} />
+        <h3 className='padding'>Speaker</h3>
         <Counter type={AutoSpeaker} change={highGoal} />
-        <h3>Amp</h3>
-        <InfoButton text={AutoAmpInfoText} />
+        <h3 className='padding'>Amp</h3>
         <Counter type={AutoAmp} change={lowGoal} />
         </>
     )
--- src/Components/teleop-page/Teleop.jsx
+++ src/Components/teleop-page/Teleop.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './Teleop.scss'
 import Stage from "./Stage";
 import Counter from "../shared/counter/Counter";
-import InfoButton from "../shared/info-buttons/InfoButtons";
+
 import { useDispatch} from "react-redux";
 import { setLowGoal, setHighGoal, setEndgame } from "../../app/Actions";
 
@@ -10,10 +10,6 @@ export default function Teleop() {
     const TeleopSpeaker = 'TeleopSpeaker'
     const TeleopAmp = 'TeleopAmp'
     const Trap ='Trap'
-    const TeleopSpeakerInfoText  = 'Click the + and - buttons to note how many Notes the robot scored in the SPEAKER during Teleop.'
-    const TeleopAmpInfoText = 'Click the + and - buttons to note how many Notes the robot scored in the AMP during Teleop.'
-    const TrapInfoText = 'Click the + and - buttons to note how many Notes the robot scored in the TRAP: maximum of 3.'
-    const StageInfoText = 'Click the appropriate button to show if the robot parked or climbed with the number of robots on the same chain.'
 
     
     const dispatch = useDispatch()
@@ -26,17 +22,13 @@ export default function Teleop() {
     return (
         <>
         <h1>Teleop</h1>
-        <h3>Speaker</h3>
-        <InfoButton text={TeleopSpeakerInfoText} />
+        <h3 className="padding">Speaker</h3>
         <Counter type={TeleopSpeaker} change={highGoal} />
-        <h3>Amp</h3>
-        <InfoButton text={TeleopAmpInfoText} />
+        <h3 className="padding">Amp</h3>
         <Counter type={TeleopAmp} change={lowGoal} />
         <h3>Trap</h3>
-        <InfoButton text={TrapInfoText} />
         <Counter type={Trap} change={trap} /> 
-        <h3>Stage</h3>
-        <InfoButton text={StageInfoText} />
+        <h3 className="padding">Stage</h3>
         <Stage />
         </>
     )
